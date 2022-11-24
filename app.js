8127ce015092e873cb3a8773846fc30b7e03b6c3
gatalsky-dv <gatalsky.d.v@gmail.com>
2022-11-24 23:24:15 +0500

    add app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const cors = require('./middlewares/cors');
+const errorHandl = require('./middlewares/errorHandl');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+mongoose.connect('mongodb://localhost:27017/moviesdb');
+
+app.use(cors);
+
+app.use(requestLogger); // подключаем логгер запросов
+
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
+
+app.use('/', require('./routes/index'));
+
+app.use(errorLogger); // подключаем логгер ошибок
+
+app.use(errors());
+
+app.use(errorHandl);
+
+app.listen(PORT);
