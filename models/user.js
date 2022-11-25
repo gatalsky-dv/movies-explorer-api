acca0d1834abc7734d0edcd08a0a229894c2c943
gatalsky-dv <gatalsky.d.v@gmail.com>
2022-11-25 12:30:02 +0500

    add models: user, movie

---
+++ models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt =require('bcryptjs');
+const Unauthorized = require('../errors/Unauthorized');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Email пользователя'],
+    unique: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  
+  password: {
+    type: String,
+    required: [true, 'Пароль пользователя'],
+    select: false,
+  },
+
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: [true, 'Имя пользователя'],
+  }
+});
+
+userSchema.statics.findUserByCredentails = function (email, password) {
+  return this
+    .findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new Unauthorized('Неправильные почта или пароль');
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new Unauthorized('Неправильные почта или пароль');
+          }
+
+          return user;
+        });
+    });
+}
+
+module.exports = mongoose.model('user', userSchema);
